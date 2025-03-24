85d7a35d113b325ea80edfa3021fe666fa0a08e3
Altyntsev Oleksii <altalex491@gmail.com>
2025-03-24 12:49:18 +0200

    fix

--- src/components/DressCode/DressCode.tsx
+++ src/components/DressCode/DressCode.tsx
@@ -5,10 +5,10 @@ import styles from "./DressCode.module.css";
 const DressCode: React.FC = () => {
   return (
     <Section id="dress-code" title="Дрес-код">
-      <a href="#" className={styles.linkBlock}>
+      <a href="/" className={styles.linkBlock}>
         Леді ♀
       </a>
-      <a href="#" className={styles.linkBlock}>
+      <a href="/" className={styles.linkBlock}>
         Джентельмени ♂
       </a>
     </Section>
